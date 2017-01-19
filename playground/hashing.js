3828d8fbfe48157afe4ec3e1933716302f9c6e6b
Sunil Chaturvedi <Sunil Chaturvedi>
2017-01-19 16:11:12 -0500

    Hash a user password middleware

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,28 @@
 // const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-const data = {
-    id: 10
-}
+const bcrypt = require('bcryptjs');
+const password = 'lakshya';
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-const token = jwt.sign(data, '123abc');
-console.log(token);
+const hashedPassword = '$2a$10$b7AGVhLFkIbi2GKwTpPBm.3eLvvkM.vMPFWsxDCUedDNvBd0i5XFe';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
 
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// const data = {
+//     id: 10
+// }
+
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // let message = 'lakshya';
 // let hash = SHA256(message).toString();
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bryptjs');
 
 let UserSchema = new mongoose.Schema({
     email: {
@@ -62,6 +63,21 @@ UserSchema.statics.findByToken = function(token) {
        'tokens.access': 'auth' 
     });
 };
+
+UserSchema.pre('save', function(next) {
+    let user = this;
+    if (user.isModified('password')) {
+        let password = user.password;
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+});
 
 let User = mongoose.model('User', UserSchema);
 
